19280945f92ed8e40a280285b199900019876317
asagamiSTI <103939634+asagamiSTI@users.noreply.github.com>
2023-04-24 13:57:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
- import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+ import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
